e13ab18265bf91263a50ea354e632cb8bf2389f4
Evan <evan.w.mclaughlin@gmail.com>
2022-07-27 10:40:57 -0400

    Include themes in dist folder

--- scripts/testArtifacts.sh
+++ scripts/testArtifacts.sh
@@ -14,6 +14,8 @@ echo -ne "  license: "
 if [ ! -f ./LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 echo -ne "  package.json: "
 if [ ! -f ./package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  blueTheme.json: "
+if [ ! -f ./themes/blueTheme.scss ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 
 echo -e "\r\n${GREEN}-----------------------------------"
 echo -e "@blui-lab/angular package successfully created"
